bbf7756214259787544f36d03b3af37998d61633
D0nald0 <muhametaj27@hotmail.com>
2021-10-28 11:41:33 +0200

    finished react course

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 //Routing
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 //components
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Movie from  "./components/Movie";
-import NotFound from  "./components/NotFound";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+//Context
+import UserProvider from "./context";
 //styles
 import {GlobalStyle} from "./GlobalStyle";
 
-const App =  () => (
+const App = () => (
     <Router>
-        <Header/>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/:movieId' element={<Movie/>} />
-            <Route path='/*' element={<NotFound/>} />
-        </Routes>
-        <GlobalStyle/>
+        <UserProvider>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/:movieId' element={<Movie/>}/>
+                <Route path='/*' element={<NotFound/>}/>
+            </Routes>
+            <GlobalStyle/>
+        </UserProvider>
     </Router>
-  );
+);
 
 
 export default App;
